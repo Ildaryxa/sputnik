0e284ca07e036fc4ac3951a4e20b27d8d57a50b5
Ildar <ildaryxa@gmail.com>
2016-06-30 01:37:33 +0500

    Перечисление типов окон

---
+++ src/main/java/enumType/FormType.java
@@ -0,0 +1,11 @@
+package enumType;
+
+/**
+ * Created by ildar on 28.06.2016.
+ */
+public enum FormType {
+        RegistrationAdmin,  //регистрация из админки
+        UpdateAdmin, //изменение из админки
+        Update, //изменить
+        Registration // регистрация
+}
